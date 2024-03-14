d8288bc2b020e9bda636df8acfc3cef60f9497e2
Francisco Gonzalez <franciscogonzalezhontoria@gmail.com>
2024-03-14 11:45:42 +0100

    delete file readme and file app

--- app.js
+++
@@ -1,9 +0,0 @@
-//con node, podemos ejecutar cualquier fichero en la terminal. Esto antes no era posible ya que solo se podia ejecutar en un navegador.
-
-//para ejecutar ficheros, usamos el comando node nombreFichero.
-
-function saludar(nombre){
-    return `Hola ${nombre}`;
-}
-
-console.log(saludar("Faku"));
